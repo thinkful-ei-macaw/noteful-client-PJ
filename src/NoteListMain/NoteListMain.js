9f4fb2c3cdec41499626479168b0c0dd8a90bf58
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-04-07 19:42:44 -0600

    taking a break

---
+++ src/NoteListMain/NoteListMain.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Note from "../Note/Note";
+import CircleButton from "../CircleButton/CircleButton";
+import ApiContext from "../ApiContext";
+import { getNotesForFolder } from "../notes-helpers";
+import "./NoteListMain.css";
+
+export default class NoteListMain extends React.Component {
+  static defaultProps = {
+    match: {
+      params: {},
+    },
+  };
+  static contextType = ApiContext;
+
+  render() {
+    const { folderId } = this.props.match.params;
+    const { notes = [] } = this.context;
+    const parsed = parseInt(folderId);
+    const notesForFolder = getNotesForFolder(notes, parsed);
+    return (
+      <section className='NoteListMain'>
+        <ul>
+          {notesForFolder.map((note) => (
+            <li key={note.id}>
+              <Note id={note.id} name={note.title} modified={note.date} />
+            </li>
+          ))}
+        </ul>
+        <div className='NoteListMain__button-container'>
+          <CircleButton
+            tag={Link}
+            to='/add-note'
+            type='button'
+            className='NoteListMain__add-note-button'
+          >
+            <FontAwesomeIcon icon='plus' />
+            <br />
+            Note
+          </CircleButton>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ src/NotePageMain/NotePageMain.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Note from "../Note/Note";
+import ApiContext from "../ApiContext";
+import { findNote } from "../notes-helpers";
+import "./NotePageMain.css";
+
+export default class NotePageMain extends React.Component {
+  static defaultProps = {
+    match: {
+      params: {},
+    },
+  };
+  static contextType = ApiContext;
+
+  handleDeleteNote = (noteId) => {
+    this.props.history.push(`/`);
+  };
+
+  render() {
+    const { notes = [] } = this.context;
+    const { noteId } = this.props.match.params;
+    const parsed = parseInt(noteId);
+    const note = findNote(notes, parsed) || { content: "" };
+
+    return (
+      <section className='NotePageMain'>
+        <Note
+          id={note.id}
+          name={note.title}
+          modified={note.date}
+          onDeleteNote={this.handleDeleteNote}
+        />
+        <div className='NotePageMain__content'>
+          {note.content.split(/\n \r|\n/).map((para, i) => (
+            <p key={i}>{para}</p>
+          ))}
+        </div>
+      </section>
+    );
+  }
+}
